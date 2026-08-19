e5a9d1c2eedd507b27d4cd563e202a21080091c0
Renato Golia <renato@golia.tech>
2026-08-19 18:36:53 +0200

    Make test models nullable-aware

--- tests/Tests.Objects/TestTypes.cs
+++ tests/Tests.Objects/TestTypes.cs
@@ -2,27 +2,27 @@ namespace Tests;
 
 public class ObjectWithSimpleProperties
 {
-    public string Text { get; set; }
+    public string? Text { get; set; }
 
     public int Value { get; set; }
 }
 
 public class ObjectWithSimpleStringArray
 {
-    public string[] Texts { get; set; }
+    public string[] Texts { get; set; } = null!;
 }
 
 public class ObjectWithSimpleIntArray
 {
-    public int[] Values { get; set; }
+    public int[] Values { get; set; } = null!;
 }
 
 public class ObjectWithComplexArray
 {
-    public ObjectWithSimpleProperties[] Items { get; set; }
+    public ObjectWithSimpleProperties[] Items { get; set; } = null!;
 }
 
 public class ObjectWithInnerObject
 {
-    public ObjectWithSimpleProperties InnerObject { get; set; }
+    public ObjectWithSimpleProperties InnerObject { get; set; } = null!;
 }
