207f64cb8a6d93437785d3342c11226416176967
Renato Golia <renato@golia.tech>
2026-08-21 14:01:44 +0200

    Remove serializer abstraction

--- src/Objects/IConfigurationSerializer.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections.Generic;
-
-namespace Kralizek.Extensions.Configuration;
-
-public interface IConfigurationSerializer
-{
-    IDictionary<string, string?> Serialize(object source, string rootSectionName);
-}
