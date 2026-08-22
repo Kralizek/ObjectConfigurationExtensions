743c5ce8f72035d1b335ddf46415007a91d51add
Renato Golia <renato@golia.tech>
2026-08-22 16:53:14 +0200

    Add configuration semantics test type

--- tests/Tests.Objects/TestTypes.cs
+++ tests/Tests.Objects/TestTypes.cs
@@ -26,3 +26,14 @@ public class ObjectWithInnerObject
 {
     public ObjectWithSimpleProperties InnerObject { get; set; } = null!;
 }
+
+public class ObjectWithNullableValues
+{
+    public string? Text { get; set; } = "Initial";
+
+    public int? Number { get; set; } = 123;
+
+    public string?[]? Values { get; set; } = ["Initial"];
+
+    public string[]? EmptyValues { get; set; } = ["Initial"];
+}
