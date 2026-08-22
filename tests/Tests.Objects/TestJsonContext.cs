5603a74237969b6a3f8efd3b735f537f4b7702da
Renato Golia <renato@golia.tech>
2026-08-22 16:53:20 +0200

    Add semantics type to source-generated context

--- tests/Tests.Objects/TestJsonContext.cs
+++ tests/Tests.Objects/TestJsonContext.cs
@@ -3,4 +3,5 @@ using System.Text.Json.Serialization;
 namespace Tests;
 
 [JsonSerializable(typeof(ObjectWithSimpleProperties))]
+[JsonSerializable(typeof(ObjectWithNullableValues))]
 internal partial class TestJsonContext : JsonSerializerContext;
