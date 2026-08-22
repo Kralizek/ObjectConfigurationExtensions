5d48c8ebb3ff99493559785bf9faa8ddb33e2e63
Renato Golia <renato@golia.tech>
2026-08-22 17:17:02 +0200

    Add focused scalar type to source-generated context

--- tests/Tests.Objects/TestJsonContext.cs
+++ tests/Tests.Objects/TestJsonContext.cs
@@ -3,4 +3,5 @@ using System.Text.Json.Serialization;
 namespace Tests;
 
 [JsonSerializable(typeof(ObjectWithStringList))]
+[JsonSerializable(typeof(ObjectWithTwoScalars))]
 internal partial class TestJsonContext : JsonSerializerContext;
