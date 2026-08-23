093d5ffefcb10f501617eba70d3fecd6fa0c3e62
Renato Golia <renato@golia.tech>
2026-08-23 22:08:25 +0200

    refactor: serialize objects directly to JSON streams

--- src/Objects/Internal/SystemTextJsonConfigurationSerializer.cs
+++ src/Objects/Internal/SystemTextJsonConfigurationSerializer.cs
@@ -1,5 +1,8 @@
+using System;
+using System.IO;
 using System.Text.Json;
 using System.Text.Json.Serialization.Metadata;
+using Microsoft.Extensions.Configuration;
 
 namespace Kralizek.Extensions.Configuration.Internal;
 
@@ -7,13 +10,47 @@ internal static class SystemTextJsonConfigurationSerializer
 {
     private static readonly JsonSerializerOptions JsonOptions = new();
 
-    public static JsonElement Serialize<T>(T source)
+    public static MemoryStream Serialize<T>(T source, string rootSectionName)
     {
-        return JsonSerializer.SerializeToElement(source, JsonOptions);
+        return Serialize(rootSectionName, writer => JsonSerializer.Serialize(writer, source, JsonOptions));
     }
 
-    public static JsonElement Serialize<T>(T source, JsonTypeInfo<T> jsonTypeInfo)
+    public static MemoryStream Serialize<T>(T source, JsonTypeInfo<T> jsonTypeInfo, string rootSectionName)
     {
-        return JsonSerializer.SerializeToElement(source, jsonTypeInfo);
+        return Serialize(rootSectionName, writer => JsonSerializer.Serialize(writer, source, jsonTypeInfo));
+    }
+
+    private static MemoryStream Serialize(string rootSectionName, Action<Utf8JsonWriter> serialize)
+    {
+        var stream = new MemoryStream();
+
+        using (var writer = new Utf8JsonWriter(stream))
+        {
+            var rootSections = GetRootSections(rootSectionName);
+
+            foreach (var rootSection in rootSections)
+            {
+                writer.WriteStartObject();
+                writer.WritePropertyName(rootSection);
+            }
+
+            serialize(writer);
+
+            for (var index = 0; index < rootSections.Length; index++)
+            {
+                writer.WriteEndObject();
+            }
+        }
+
+        stream.Position = 0;
+
+        return stream;
+    }
+
+    private static string[] GetRootSections(string rootSectionName)
+    {
+        return string.IsNullOrEmpty(rootSectionName)
+            ? Array.Empty<string>()
+            : rootSectionName.Split(new[] { ConfigurationPath.KeyDelimiter }, StringSplitOptions.None);
     }
 }
